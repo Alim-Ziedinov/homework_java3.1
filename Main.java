c352ece6132322bb6b06fd0a1c0966b9d796f0ab
Alim-ziedinov <a-zidan@rambler.ru>
2021-09-21 17:01:54 +0300

    nev_file_Main

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price_ticket = 10_000;
+        int bonus = 20;
+        int total_bonus = price_ticket / bonus;
+        System.out.print(total_bonus);
+    }
+}
