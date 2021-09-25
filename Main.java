ed1887b87d40fafbc7a73ecc82e3690903873f75
Alim-Ziedinov <87672970+Alim-Ziedinov@users.noreply.github.com>
2021-09-25 23:55:32 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int priceTicket = 10_000;
         int bonus = 20;
-        int total_bonus = priceTicket / bonus;
-        System.out.print(total_bonus);
+        int totalBonus = priceTicket / bonus;
+        System.out.print(totalBonus);
     }
 }
